6a63536cfe57279e143ec46582363675d759a205
aseelflihan <aseelflihan@gmail.com>
2023-01-24 17:20:11 +0300

    complated assginment

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 function addingEventListener() {
-}
+    const clickAlert = () => {
+      alert("I was clicked!");
+    };
+    const inp = document.querySelector("input");
+    inp.addEventListener("click", clickAlert);
+  }
